d4ce4ea5a12f169cb2c6db3ed4b5bb6b23a82712
Gopal <gopalkrishnapanda0007@gmail.com>
2022-06-07 00:21:45 +0530

    background color and fonts

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const pic = document.getElementsByClassName("pic");
 var id = 0;
 
 const users = [
-    {name: "Gopal", about : "Hi I am gopal. A software eng", pic :"\images\ss.png"},
-    {name: "Ronnie", about : "Hi I am Ronnie. A software eng", pic :"\images\ss.png"},
-    {name: "Arnab", about : "Hi I am Arnab. A software eng", pic :"\images\ss.png"},
-    {name: "Ashish", about : "Hi I am Ashish. A software eng", pic :"\images\ss.png"},
-    {name: "Abhijeet", about : "Hi I am Abhijeet. A software eng", pic :"\images\ss.png"}
+    { name: "Gopal", about: '" Complete Package with seemingly unlimited potential! "', pic: "\images\ss.png" },
+    { name: "Ronnie", about: '" Best place for marketing and advertising "', pic: "\images\ss.png" },
+    { name: "Arnab", about: '" User.com is best website for marketing "', pic: "\images\ss.png" },
+    { name: "Ashish", about: '" User.com is a superb all-in-one marketing automation tool "', pic: "\images\ss.png" },
+    { name: "Abhijeet", about: '" Sales & marketing tool for B2B companies "', pic: "\images\ss.png" }
 ];
 
-var flipper = function(id){
+var flipper = function (id) {
     console.log(`${id}`)
     review.innerHTML = users[id].about;
     name.innerHTML = users[id].name;
@@ -24,19 +24,19 @@ var flipper = function(id){
 
 };
 
-prev.addEventListener('click' , function(){
-    id = id - 1 ; 
-    if(id < 0) { id = (users.length -1) ;}
+prev.addEventListener('click', function () {
+    id = id - 1;
+    if (id < 0) { id = (users.length - 1); }
     flipper(id);
 });
 
-next.addEventListener('click' , function(){
-    id = id + 1 ; 
-    if(id > (users.length - 1)) { id = 0;}
+next.addEventListener('click', function () {
+    id = id + 1;
+    if (id > (users.length - 1)) { id = 0; }
     flipper(id);
 });
 
-random.addEventListener('click' , function(){
+random.addEventListener('click', function () {
     id = Math.floor(Math.random() * (users.length))
     flipper(id);
 });
