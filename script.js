5d88c18e652f851e6f817561797266c1dfb35e66
Gopal <gopalkrishnapanda0007@gmail.com>
2022-06-08 00:49:10 +0530

    Finishing touch

--- script.js
+++ script.js
@@ -8,20 +8,17 @@ const pic = document.getElementsByClassName("pic");
 var id = 0;
 
 const users = [
-    { name: "Gopal", about: '" Complete Package with seemingly unlimited potential! "', pic: "\images\ss.png" },
-    { name: "Ronnie", about: '" Best place for marketing and advertising "', pic: "\images\ss.png" },
-    { name: "Arnab", about: '" User.com is best website for marketing "', pic: "\images\ss.png" },
-    { name: "Ashish", about: '" User.com is a superb all-in-one marketing automation tool "', pic: "\images\ss.png" },
-    { name: "Abhijeet", about: '" Sales & marketing tool for B2B companies "', pic: "\images\ss.png" }
+    { name: "Gopal", about: '" Complete Package with seemingly unlimited potential! "', pic: "images/gopal.jpg" },
+    { name: "Ronnie", about: '" Best place for marketing and advertising "', pic: 'images/ronnie.JPG' },
+    { name: "Arnab", about: '" User.com is best website for marketing "', pic: "/images/arnab.jpg" },
+    { name: "Ashish", about: '" User.com is a superb all-in-one marketing automation tool "', pic: "/images/ashish.JPG" },
+    { name: "Abhijeet", about: '" Sales & marketing tool for B2B companies "', pic: "/images/abhijeet.JPG" }
 ];
 
 var flipper = function (id) {
-    console.log(`${id}`)
     review.innerHTML = users[id].about;
     name.innerHTML = users[id].name;
-    pic.src = users[id].pic
-
-
+    document.getElementById("img").src = users[id].pic
 };
 
 prev.addEventListener('click', function () {
